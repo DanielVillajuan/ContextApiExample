a4b7e43216912de1f460bd0e03a20197b3088bcd
Daniel Villajuan <danielvillajuan@jelou.ai>
2023-03-29 14:48:47 -0300

    fix deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import PersonajeA from './PersonajeA';
 import PersonajeB from './PersonajeB';
--- src/PersonajeB.js
+++ src/PersonajeB.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { UserContext } from './context/UserContext'
 
 function PersonajeB() {
-    const {user ,setUser} = useContext(UserContext)
+    const { user } = useContext(UserContext)
   return (
     <div>PersonajeB {user}</div>
   )
